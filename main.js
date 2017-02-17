0392375414aa66f180a72c094f9d0cb15ae928c0
Kevin Unbedacht <KCUimac@Kevins-iMac-16306.local>
2017-02-17 11:49:25 -0700

    finished wire frame

---
+++ main.js
@@ -0,0 +1,71 @@
+
+// variables
+var $header_top = $('.header-top');
+var $nav = $('nav');
+
+
+
+// toggle menu
+$header_top.find('a').on('click', function() {
+  $(this).parent().toggleClass('open-menu');
+});
+
+
+
+// fullpage customization
+$('#fullpage').fullpage({
+  sectionsColor: ['white', 'white', 'white', 'white', 'white'],
+  sectionSelector: '.vertical-scrolling',
+  slideSelector: '.horizontal-scrolling',
+  navigation: false,
+  slidesNavigation: true,
+  controlArrows: false,
+  verticalCentered: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection'],
+  menu: '#menu',
+
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'rgba(0, 47, 77, .3)');
+    $nav.css('background', 'rgba(0, 47, 77, .25)');
+    console.log("afterLoad", index);
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
+
+  onLeave: function(index, nextIndex, direction) {
+    console.log("onLeave", index);
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+
+  afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
+    console.log("afterSlideLoad", index, "slideIndex", slideIndex);
+    if(anchorLink == 'secondSection' && slideIndex == 1) {
+      $.fn.fullpage.setAllowScrolling(false, 'up');
+      $header_top.css('background', 'transparent');
+      $nav.css('background', 'transparent');
+      $(this).css('background', 'white');
+      $(this).find('h2').css('color', 'black');
+      $(this).find('h3').css('color', 'black');
+      $(this).find('p').css(
+        {
+          'color': 'black',
+          'opacity': .6
+          // 'transform': 'translateY(0)'
+        }
+      );
+    }
+  },
+
+  onSlideLeave: function( anchorLink, index, slideIndex, direction) {
+    console.log("onSlideLeave", index, "slideIndex", slideIndex);
+    if(index == 2 && slideIndex == 1) {
+      $.fn.fullpage.setAllowScrolling(true, 'up');
+      $header_top.css('background', 'rgba(0, 47, 77, .3)');
+      $nav.css('background', 'rgba(0, 47, 77, .25)');
+      console.log("yay");
+    }
+  }
+});
